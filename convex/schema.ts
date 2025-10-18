a86899a91b763d3fda16bf31c75d2bbfd1326a61
Alexander Knipfer <alexanderknipfer@gmail.com>
2025-10-18 16:22:15 -0400

    update schema and add queries for getting vehicles and create vehicle mutation

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,18 +3,20 @@ import { v } from 'convex/values';
 
 export default defineSchema({
   vehicles: defineTable({
-    id: v.id('vehicles'),
     name: v.optional(v.string()),
     make: v.string(),
     model: v.string(),
     year: v.string(),
     plate: v.string(),
     imageUrl: v.optional(v.string()),
-  }),
+    userId: v.string(),
+  }).index('by_userid', ['userId']),
   vehicle_models: defineTable({
     body_styles: v.array(v.string()),
     make: v.string(),
-    model: v.union(v.float64(), v.string()),
+    model: v.string(),
     year: v.float64(),
-  }).index('by_year', ['year']),
+  })
+    .index('by_year', ['year'])
+    .index('by_year_make', ['year', 'make']),
 });
--- convex/vehicles.ts
+++ convex/vehicles.ts
@@ -1,5 +1,5 @@
 import { v } from 'convex/values';
-import { query } from './_generated/server';
+import { query, mutation } from './_generated/server';
 
 export const getVehicleMakesByYear = query({
   args: {
@@ -26,5 +26,77 @@ export const getVehicleMakesByYear = query({
           .map((vehicle) => vehicle.make),
       ),
     );
+  },
+});
+
+export const getVehicleModelsByYearAndMake = query({
+  args: {
+    year: v.float64(),
+    make: v.string(),
+  },
+  handler: async (ctx, { year, make }) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new Error(
+        'Unauthorized: User identity is required to access this data.',
+      );
+    }
+
+    const vehicles = await ctx.db
+      .query('vehicle_models')
+      .withIndex('by_year_make', (q) => q.eq('year', year).eq('make', make))
+      .collect();
+
+    return vehicles.map((vehicle) => vehicle.model);
+  },
+});
+
+export const createUserVehicle = mutation({
+  args: {
+    name: v.optional(v.string()),
+    make: v.string(),
+    model: v.string(),
+    year: v.string(),
+    plate: v.string(),
+  },
+  handler: async (ctx, { name, make, model, year, plate }) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new Error(
+        'Unauthorized: User identity is required to access this data.',
+      );
+    }
+
+    const inserted = ctx.db.insert('vehicles', {
+      name,
+      make,
+      model,
+      year,
+      plate,
+      userId: identity.tokenIdentifier,
+    });
+
+    return inserted;
+  },
+});
+
+export const getUserVehicles = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new Error(
+        'Unauthorized: User identity is required to access this data.',
+      );
+    }
+
+    const vehicles = await ctx.db
+      .query('vehicles')
+      .withIndex('by_userid', (q) => q.eq('userId', identity.tokenIdentifier))
+      .collect();
+
+    return vehicles;
   },
 });
