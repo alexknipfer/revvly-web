3ed5e840948843cb2c3b008285a0cde6a21a33e1
Alex <alexanderknipfer@gmail.com>
2025-12-19 14:35:39 -0500

    handle convex errors globally

--- src/router.tsx
+++ src/router.tsx
@@ -7,6 +7,8 @@ import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query
 import { appConfig } from '@/lib/appConfig';
 
 import { routeTree } from './routeTree.gen';
+import { toast } from 'sonner';
+import { ConvexError } from 'convex/values';
 
 export function getRouter() {
   const convex = new ConvexReactClient(appConfig.convex.url, {
@@ -20,6 +22,17 @@ export function getRouter() {
       queries: {
         queryKeyHashFn: convexQueryClient.hashFn(),
         queryFn: convexQueryClient.queryFn(),
+      },
+      mutations: {
+        onError: (error) => {
+          if (error instanceof ConvexError) {
+            toast.error(error.data.message);
+          } else if (error instanceof Error) {
+            toast.error(error.message);
+          } else {
+            toast.error('An unknown error occurred');
+          }
+        },
       },
     },
   });
