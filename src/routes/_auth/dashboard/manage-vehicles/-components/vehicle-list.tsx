a26698865608aab9533063e60847a509948ecea3
Alex Knipfer <alexanderknipfer@gmail.com>
2025-12-19 23:26:52 -0500

    fill three columns on medium size

--- src/routes/_auth/dashboard/manage-vehicles/-components/vehicle-list.tsx
+++ src/routes/_auth/dashboard/manage-vehicles/-components/vehicle-list.tsx
@@ -51,7 +51,7 @@ export function VehicleList() {
     );
   }
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-[repeat(auto-fit,400px)] gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-[repeat(auto-fit,400px)] gap-4">
       {vehicles.map((vehicle) => (
         <VehicleDetails key={vehicle._id} vehicle={vehicle} />
       ))}
