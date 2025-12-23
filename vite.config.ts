ea1fb50aa19c87735167e4056bfecc8949ec4e50
Alex <alexanderknipfer@gmail.com>
2025-12-23 15:29:55 -0500

    revert to use netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,13 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import { cloudflare } from '@cloudflare/vite-plugin';
+import netlify from '@netlify/vite-plugin-tanstack-start';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    tsConfigPaths(),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
   // To fix the error "The requested module does not provide an export named 'parse'"
   optimizeDeps: {
     include: ['@clerk/tanstack-react-start', 'cookie-es'],
