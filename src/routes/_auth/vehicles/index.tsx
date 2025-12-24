4ce69405e8a74d163ce682fb9eaff969cbf6344c
Alex <alexanderknipfer@gmail.com>
2025-12-23 23:47:55 -0500

    prefetch user vehicle by id

--- src/routes/_auth/vehicles/index.tsx
+++ src/routes/_auth/vehicles/index.tsx
@@ -1,10 +1,19 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 import { VehiclesView } from '@/modules/vehicles/ui/views/vehicles-view';
+import { convexQuery } from '@convex-dev/react-query';
+import { api } from 'convex/_generated/api';
 
 export const Route = createFileRoute('/_auth/vehicles/')({
   component: RouteComponent,
   loader: async ({ context }) => {
     if (context.vehicles.length) {
+      const firstVehicle = context.vehicles[0];
+      context.queryClient.prefetchQuery(
+        convexQuery(api.userVehicles.getById, {
+          id: firstVehicle._id,
+        }),
+      );
+
       throw redirect({
         to: '/vehicles/$vehicleId',
         params: {
