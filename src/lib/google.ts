108304cc656dd95117f4338e06fc208644aec242
Alex Knipfer <alexanderknipfer@gmail.com>
2025-12-26 21:42:42 -0600

    use client app config for google key

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,13 +1,13 @@
 import { createServerOnlyFn } from '@tanstack/react-start';
 import ky from 'ky';
 
-import { serverAppConfig } from '@/lib/appConfig';
+import { appConfig } from '@/lib/appConfig';
 
 export const googlePlacesApi = createServerOnlyFn(() =>
   ky.create({
     prefixUrl: 'https://places.googleapis.com/v1/places',
     headers: {
-      'X-Goog-Api-Key': serverAppConfig().googleMaps.apiKey,
+      'X-Goog-Api-Key': appConfig.googleMaps.apiKey,
     },
   }),
 );
