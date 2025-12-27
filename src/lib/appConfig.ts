ed31ddeddc65ee072ea2dcd5354601301bc4c8f0
Alex Knipfer <alexanderknipfer@gmail.com>
2025-12-26 21:32:31 -0600

    remove google api key from client app config

--- src/lib/appConfig.ts
+++ src/lib/appConfig.ts
@@ -23,9 +23,6 @@ export const appConfig = {
   convex: {
     url: loadEnvironmentVariable('VITE_CONVEX_URL'),
   },
-  googleMaps: {
-    apiKey: loadEnvironmentVariable('VITE_GOOGLE_MAPS_API_KEY'),
-  },
 };
 
 export const serverAppConfig = createServerOnlyFn(() => ({
