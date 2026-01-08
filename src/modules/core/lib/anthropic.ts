073f92d0af653731471f4a19c5992630a1275242
Alex <alexanderknipfer@gmail.com>
2026-01-08 15:03:58 -0500

    log key

--- src/modules/core/lib/anthropic.ts
+++ src/modules/core/lib/anthropic.ts
@@ -4,6 +4,8 @@ import { createServerOnlyFn } from '@tanstack/react-start';
 import { serverAppConfig } from '@/modules/core/lib/appConfig';
 
 export const anthropicHaikuAdapter = createServerOnlyFn(() => {
+  console.log('serverAppConfig: ', serverAppConfig().anthropic.apiKey);
+
   return createAnthropicChat(
     'claude-haiku-4-5',
     serverAppConfig().anthropic.apiKey,
