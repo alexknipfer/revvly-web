2efeeb29f5f047392d8678cb976c6fe048c999a5
Alex <alexanderknipfer@gmail.com>
2026-01-12 15:24:10 -0500

    add setMissedFuelupDefault to migration runner

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,9 +1,12 @@
 import { Migrations } from '@convex-dev/migrations';
-import { components } from './_generated/api.js';
+
+import { components, internal } from './_generated/api.js';
 import { DataModel } from './_generated/dataModel.js';
 
 export const migrations = new Migrations<DataModel>(components.migrations);
-export const run = migrations.runner();
+export const run = migrations.runner([
+  internal.migrations.setMissedFuelupDefault,
+]);
 
 export const setMissedFuelupDefault = migrations.define({
   table: 'fuel_entries',
