9ce25d53f1df485d212e92238f3bbf6085a548ba
Alex <alexanderknipfer@gmail.com>
2026-01-13 14:21:38 -0500

    testing nitro deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-import netlify from '@netlify/vite-plugin-tanstack-start';
+// import netlify from '@netlify/vite-plugin-tanstack-start';
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
+  plugins: [tsConfigPaths(), tanstackStart(), nitro(), viteReact()],
   // To fix the error "The requested module does not provide an export named 'parse'"
   optimizeDeps: {
     include: ['@clerk/tanstack-react-start', 'cookie-es'],
