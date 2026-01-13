9da4f6d382df24cf8a352e0ca757ddb7d62504b3
Alex <alexanderknipfer@gmail.com>
2026-01-13 16:13:28 -0500

    move receipt upload to client

--- convex/storage.ts
+++ convex/storage.ts
@@ -1,7 +1,22 @@
+import { v } from 'convex/values';
 import { mutation } from './_generated/server';
 
 export const generateUploadUrl = mutation({
   handler: (ctx) => {
     return ctx.storage.generateUploadUrl();
+  },
+});
+
+export const getFileUrl = mutation({
+  args: { storageId: v.id('_storage') },
+  handler: async (ctx, args) => {
+    return ctx.storage.getUrl(args.storageId);
+  },
+});
+
+export const deleteFile = mutation({
+  args: { storageId: v.id('_storage') },
+  handler: (ctx, args) => {
+    return ctx.storage.delete(args.storageId);
   },
 });
