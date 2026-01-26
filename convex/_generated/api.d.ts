74cc14f66a2345f2646beae580259243778f89de
Alex <alexanderknipfer@gmail.com>
2026-01-26 14:32:04 -0500

    rename fuelly import convex function

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,7 +10,7 @@
 
 import type * as fuelEntries from "../fuelEntries.js";
 import type * as fuelEntries_validators from "../fuelEntries/validators.js";
-import type * as import_ from "../import.js";
+import type * as importFuellyData from "../importFuellyData.js";
 import type * as migrations from "../migrations.js";
 import type * as services from "../services.js";
 import type * as services_validators from "../services/validators.js";
@@ -29,7 +29,7 @@ import type {
 declare const fullApi: ApiFromModules<{
   fuelEntries: typeof fuelEntries;
   "fuelEntries/validators": typeof fuelEntries_validators;
-  import: typeof import_;
+  importFuellyData: typeof importFuellyData;
   migrations: typeof migrations;
   services: typeof services;
   "services/validators": typeof services_validators;
