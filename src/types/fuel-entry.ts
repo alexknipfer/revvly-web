302b750e8e76f862c59fcaf623180bb3b91eba38
Alex <alexanderknipfer@gmail.com>
2026-01-26 15:33:17 -0500

    remove catch() causing error when getting options in fuel entry form

--- src/types/fuel-entry.ts
+++ src/types/fuel-entry.ts
@@ -2,12 +2,10 @@ import { zid } from 'convex-helpers/server/zod4';
 import z from 'zod';
 
 export type FuelType = z.infer<typeof fuelTypeSchema>;
-export const fuelTypeSchema = z
-  .enum(['regular', 'premium', 'diesel', 'e85'])
-  .catch('regular');
+export const fuelTypeSchema = z.enum(['regular', 'premium', 'diesel', 'e85']);
 
 export type FuelLevel = z.infer<typeof fuelLevelSchema>;
-export const fuelLevelSchema = z.enum(['full', 'partial']).catch('full');
+export const fuelLevelSchema = z.enum(['full', 'partial']);
 
 export const fuellyFuelEntryImportSchema = z.object({
   date: z.string(),
