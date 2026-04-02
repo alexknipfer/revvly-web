4dcae291bbcd2510b8135942d41396b1a061f4ac
Alex Knipfer <alexanderknipfer@gmail.com>
2026-04-01 22:16:01 -0400

    remove vite tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -9,14 +8,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tsConfigPaths(),
-    tailwindcss(),
-    tanstackStart(),
-    nitro(),
-    viteReact(),
-  ],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
   optimizeDeps: {
     include: ['@clerk/tanstack-react-start', 'cookie-es'],
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 });
